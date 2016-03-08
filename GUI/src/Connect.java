13d78f1a158ba78e026531941b7e0769219be8fa
cetona <jacobpettersson92@live.se>
2016-03-08 23:18:52 +0100

    GUI

---
+++ GUI/src/Connect.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Jacob
+ */
+public class Connect {
+    Connection conn = null;
+    
+    public static Connection ConnectDB(){
+        try  {Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite: init_databas.sql");
+        JOptionPane.showMessageDialog(null, "Connected to registry");
+        
+        return conn;
+        } catch(Exception e) {
+           JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+}
